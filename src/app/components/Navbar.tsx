07653aeaa240a0d056195a9291c2eb12716a3579
WoraponK <worapon.klabsri@gmail.com>
2024-02-29 22:31:04 +0700

    fix: ' error

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -110,7 +110,7 @@ function Navbar() {
             </ul>
             <button className='bg-clr-secondary-1 py-2 px-4 max-sm:p-3 text-lg rounded-full flex items-center  gap-1 transition-colors hover:bg-clr-secondary-2'>
                 <MoodIcon level={5} size={25} />
-                <span className='max-sm:hidden'>How'bout day?</span>
+                <span className='max-sm:hidden'>How bout day?</span>
             </button>
         </nav>
     )
