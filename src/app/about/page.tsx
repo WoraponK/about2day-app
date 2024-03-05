ef6b3fe800ba8ff2b2076a726bc374dc0292a41c
WoraponK <worapon.klabsri@gmail.com>
2024-03-05 07:23:48 +0700

    update:2024-03-05 #10

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,10 +24,12 @@ function AboutPage() {
         <h3 className='text-3xl font-semibold text-center'>
           <LanguageSwap en='What is about2day ?' th='อะไรคือ about2day' />
         </h3>
-        <p>
+        <p className='text-lg'>
           <LanguageSwap
-            en=''
-            th='โดยเริ่มต้นแล้ว abou2day ได้มีดีไซน์และเป้าหมายในการสร้างเป็นแอปพลิเคชันบนมือถือ แต่ในปัจจุบันมีความคิดที่อยากจะทำเป็นเว็บแอปพลิเคชันเลยได้มีโอกาสสร้างเว็บไซต์นี้ขึ้นมา แต่ถ้าถามว่ายังอยากสร้างเป็นแอปพลิเคชันบนมือถือไหม ? แน่นอนว่าเรายังต้องการที่จะสร้างมันขึ้นมา'
+            en='
+            About2day was initially designed and developed as a mobile application. However, we are now considering the possibility of expanding it into a web application, which is why we have created this website.
+            To answer your question, yes, we still have a strong desire to develop abou2day as a mobile application in the future.'
+            th='โดยเริ่มต้นแล้ว about2day ได้มีดีไซน์และเป้าหมายในการสร้างเป็นแอปพลิเคชันบนมือถือ แต่ในปัจจุบันมีความคิดที่อยากจะทำเป็นเว็บแอปพลิเคชันเลยได้มีโอกาสสร้างเว็บไซต์นี้ขึ้นมา แต่ถ้าถามว่ายังอยากสร้างเป็นแอปพลิเคชันบนมือถือไหม ? แน่นอนว่าเรายังต้องการที่จะสร้างมันขึ้นมา'
           />
         </p>
       </section>
@@ -35,9 +37,9 @@ function AboutPage() {
         <h3 className='text-3xl font-semibold text-center'>
           <LanguageSwap en='Why about2day ?' th='ทำไมต้อง about2day' />
         </h3>
-        <p>
+        <p className='text-lg'>
           <LanguageSwap
-            en=''
+            en='Do you want to keep track of your daily activities? If so, our website can help you. Our main feature is "Income-Expenses" recording, but thats not all. You can also create your own records, which can be in the form of number of times or satisfaction level.'
             th='ต้องการบันทึกข้อมูลในแต่ละวันไหมล่ะ ? ถ้าใช่ เว็บไซต์ของเราสามารถตอบโจทย์ให้คุณได้ โดยการบันทึกหลักของเราก็คือ "รายรับ-รายจ่าย" แต่ไม่ได้มีแค่นั้น คุณยังสามารถที่จะสร้างการบันทึกของคุณมาได้ แต่จะเป็นการบันทึกในรูปแบบจำนวนครั้งหรือความพึงพอใจ'
           />
         </p>
@@ -46,9 +48,9 @@ function AboutPage() {
         <h3 className='text-3xl font-semibold text-center'>
           <LanguageSwap en='What is "BoutDay?" Button' th='อะไรคือปุ่ม "วันนี้เป็นไง?"' />
         </h3>
-        <p>
+        <p className='text-lg'>
           <LanguageSwap
-            en=''
+            en='How do you feel in everyday? You can record your daily feel.'
             th='ในแต่ละวันคุณรู้สึกพึงพอใจมากแค่ไหนกันล่ะ ? คุณสามารถบันทึกความพึงพอใจในแต่ละวันได้'
           />
         </p>
