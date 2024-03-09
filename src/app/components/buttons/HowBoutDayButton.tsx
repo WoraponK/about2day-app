c821836a5af033ddf7c91ede3211c388c145ec9a
WoraponK <worapon.klabsri@gmail.com>
2024-03-10 00:53:39 +0700

    update:2024-03-09 #6

--- src/app/components/buttons/HowBoutDayButton.tsx
+++ src/app/components/buttons/HowBoutDayButton.tsx
@@ -4,6 +4,7 @@ import MoodIcon from '../icons/MoodIcon'
 
 function HowBoutDayButton() {
     const [language, setLanguage] = useState('');
+    const [isDisabled, setIsDisabled] = useState(true);
 
     useEffect(() => {
         const languageCheck = () => {
@@ -13,16 +14,30 @@ function HowBoutDayButton() {
             }
         }
 
+        const updateButtonState = () => {
+            const currentHour = new Date().getHours();
+            const currentMinute = new Date().getMinutes();
+
+            setIsDisabled(currentHour < 22 || (currentHour === 22 && currentMinute >= 0) || currentHour >= 0)
+        }
+
         languageCheck();
+        updateButtonState();
+
+        const intervalId = setInterval(updateButtonState, 60000);
+        return () => clearInterval(intervalId);
     }, [])
 
     return (
-        <button className='bg-clr-secondary-1 py-2 px-4 max-sm:p-3 text-lg rounded-full flex items-center  space-x-3 transition-all active:scale-95 hover:bg-clr-secondary-2'>
+        <button
+            className='bg-clr-secondary-1 py-2 px-4 max-sm:p-3 text-lg rounded-full flex items-center  space-x-3 transition-all active:scale-95 hover:bg-clr-secondary-2 disabled:bg-clr-light/70 disabled:cursor-not-allowed disabled:active:scale-100'
+            disabled={isDisabled}
+        >
             <MoodIcon level={5} size={25} />
             <span className='max-sm:hidden font-semibold'>
                 {language === 'en'
-                ? 'BoutDay?'
-                : 'วันนี้เป็นไง?'}
+                    ? 'BoutDay?'
+                    : 'วันนี้เป็นไง?'}
             </span>
         </button>
     )
